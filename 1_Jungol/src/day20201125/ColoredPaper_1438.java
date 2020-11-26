e98f8348ca2dda3b5bf1ec7a4dcf268d9f94691f
eeeeyeot <ghfuf12@naver.com>
2020-11-26 18:47:47 +0900

    2020_11_26

--- 1_Jungol/src/day20201125/ColoredPaper_1438.java
+++ 1_Jungol/src/day20201125/ColoredPaper_1438.java
@@ -9,15 +9,27 @@ public class ColoredPaper_1438
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		boolean[][] arr = new boolean[100][100];
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine().trim());
 		int cnt = 0;
+
 		
 		for(int i = 0; i < T; i++) {
-			String[] str = br.readLine().split(" ");
-			int n = Integer.parseInt(str[0]);
-			int m = Integer.parseInt(str[1]);
+			String[] str = br.readLine().trim().split(" ");
+			int n = Integer.parseInt(str[0]) - 1;
+			int m = Integer.parseInt(str[1]) - 1;
 			
-			//for(int x = 0; x < n)
+			for(int x = n; x < n + 10; x++) {
+				for(int y = m; y < m + 10; y++) {
+					arr[x][y] = true;
+				}
+			}
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				if(arr[i][j])
+					cnt++;
+			}
 		}
 		
 		System.out.println(cnt);
